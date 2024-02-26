af514b0acc9d4342474722fb165cba33b925fbb7
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-02-26 15:31:46 +0100

    Update test_video_ava.py

--- test_video_ava.py
+++ test_video_ava.py
@@ -43,6 +43,106 @@ def parse_args():
     return parser.parse_args()
                     
 
+# @torch.no_grad()
+# def run(args, d_cfg, model, device, transform, class_names):
+#     # path to save 
+#     save_path = os.path.join(args.save_folder, 'ava_video')
+#     os.makedirs(save_path, exist_ok=True)
+
+#     # path to video
+#     path_to_video = os.path.join(d_cfg['data_root'], 'videos_15min', args.video)
+
+#     # video
+#     video = cv2.VideoCapture(path_to_video)
+#     fourcc = cv2.VideoWriter_fourcc(*'XVID')
+#     save_size = (640, 480)
+#     save_name = os.path.join(save_path, 'detection.avi')
+#     fps = 15.0
+#     out = cv2.VideoWriter(save_name, fourcc, fps, save_size)
+
+#     video_clip = []
+#     while(True):
+#         ret, frame = video.read()
+        
+#         if ret:
+#             # to PIL image
+#             frame_pil = Image.fromarray(frame.astype(np.uint8))
+
+#             # prepare
+#             if len(video_clip) <= 0:
+#                 for _ in range(d_cfg['len_clip']):
+#                     video_clip.append(frame_pil)
+
+#             video_clip.append(frame_pil)
+#             del video_clip[0]
+
+#             # orig size
+#             orig_h, orig_w = frame.shape[:2]
+
+#             # transform
+#             x, _ = transform(video_clip)
+#             # List [T, 3, H, W] -> [3, T, H, W]
+#             x = torch.stack(x, dim=1)
+#             x = x.unsqueeze(0).to(device) # [B, 3, T, H, W], B=1
+
+#             t0 = time.time()
+#             # inference
+#             batch_bboxes = model(x)
+#             print("inference time ", time.time() - t0, "s")
+
+#             # batch size = 1
+#             bboxes = batch_bboxes[0]
+
+#             # visualize detection results
+#             for bbox in bboxes:
+#                 x1, y1, x2, y2 = bbox[:4]
+#                 det_conf = float(bbox[4])
+#                 cls_out = [det_conf * cls_conf for cls_conf in bbox[5:]]
+            
+#                 # rescale bbox
+#                 x1, x2 = int(x1 * orig_w), int(x2 * orig_w)
+#                 y1, y2 = int(y1 * orig_h), int(y2 * orig_h)
+
+#                 cls_scores = np.array(cls_out)
+#                 indices = np.where(cls_scores > 0.4)
+#                 scores = cls_scores[indices]
+#                 indices = list(indices[0])
+#                 scores = list(scores)
+
+#                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+#                 if len(scores) > 0:
+#                     blk   = np.zeros(frame.shape, np.uint8)
+#                     font  = cv2.FONT_HERSHEY_SIMPLEX
+#                     coord = []
+#                     text  = []
+#                     text_size = []
+
+#                     for _, cls_ind in enumerate(indices):
+#                         text.append("[{:.2f}] ".format(scores[_]) + str(class_names[cls_ind]))
+#                         text_size.append(cv2.getTextSize(text[-1], font, fontScale=0.25, thickness=1)[0])
+#                         coord.append((x1+3, y1+7+10*_))
+#                         cv2.rectangle(blk, (coord[-1][0]-1, coord[-1][1]-6), (coord[-1][0]+text_size[-1][0]+1, coord[-1][1]+text_size[-1][1]-4), (0, 255, 0), cv2.FILLED)
+#                     frame = cv2.addWeighted(frame, 1.0, blk, 0.25, 1)
+#                     for t in range(len(text)):
+#                         cv2.putText(frame, text[t], coord[t], font, 0.25, (0, 0, 0), 1)
+
+#             # save
+#             out.write(frame)
+
+#             if args.show:
+#                 # show
+#                 cv2.imshow('key-frame detection', frame)
+#                 cv2.waitKey(1)
+
+#         else:
+#             break
+
+#     video.release()
+#     out.release()
+#     #cv2.destroyAllWindows()
+
+
 @torch.no_grad()
 def run(args, d_cfg, model, device, transform, class_names):
     # path to save 
@@ -54,16 +154,18 @@ def run(args, d_cfg, model, device, transform, class_names):
 
     # video
     video = cv2.VideoCapture(path_to_video)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    save_size = (640, 480)
-    save_name = os.path.join(save_path, 'detection.avi')
-    fps = 15.0
-    out = cv2.VideoWriter(save_name, fourcc, fps, save_size)
+    fourcc = cv2.VideoWriter_fourcc(*'FMP4')
 
+    save_size = (640, 480)
+    save_name = os.path.join(save_path, 'detection.gif')  # Change extension to .gif
+    fps = 15.0
+
+    # Create a list to store frames for gif creation
+    gif_frames = []
     video_clip = []
     while(True):
         ret, frame = video.read()
-        
+        i = 0
         if ret:
             # to PIL image
             frame_pil = Image.fromarray(frame.astype(np.uint8))
@@ -83,7 +185,7 @@ def run(args, d_cfg, model, device, transform, class_names):
             x, _ = transform(video_clip)
             # List [T, 3, H, W] -> [3, T, H, W]
             x = torch.stack(x, dim=1)
-            x = x.unsqueeze(0).to(device) # [B, 3, T, H, W], B=1
+            x = x.unsqueeze(0).to(device)  # [B, 3, T, H, W], B=1
 
             t0 = time.time()
             # inference
@@ -98,7 +200,7 @@ def run(args, d_cfg, model, device, transform, class_names):
                 x1, y1, x2, y2 = bbox[:4]
                 det_conf = float(bbox[4])
                 cls_out = [det_conf * cls_conf for cls_conf in bbox[5:]]
-            
+
                 # rescale bbox
                 x1, x2 = int(x1 * orig_w), int(x2 * orig_w)
                 y1, y2 = int(y1 * orig_h), int(y2 * orig_h)
@@ -112,35 +214,39 @@ def run(args, d_cfg, model, device, transform, class_names):
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
                 if len(scores) > 0:
-                    blk   = np.zeros(frame.shape, np.uint8)
-                    font  = cv2.FONT_HERSHEY_SIMPLEX
+                    blk = np.zeros(frame.shape, np.uint8)
+                    font = cv2.FONT_HERSHEY_SIMPLEX
                     coord = []
-                    text  = []
+                    text = []
                     text_size = []
 
                     for _, cls_ind in enumerate(indices):
                         text.append("[{:.2f}] ".format(scores[_]) + str(class_names[cls_ind]))
                         text_size.append(cv2.getTextSize(text[-1], font, fontScale=0.25, thickness=1)[0])
-                        coord.append((x1+3, y1+7+10*_))
-                        cv2.rectangle(blk, (coord[-1][0]-1, coord[-1][1]-6), (coord[-1][0]+text_size[-1][0]+1, coord[-1][1]+text_size[-1][1]-4), (0, 255, 0), cv2.FILLED)
+                        coord.append((x1 + 3, y1 + 7 + 10 * _))
+                        cv2.rectangle(blk, (coord[-1][0] - 1, coord[-1][1] - 6),
+                                      (coord[-1][0] + text_size[-1][0] + 1, coord[-1][1] + text_size[-1][1] - 4),
+                                      (0, 255, 0), cv2.FILLED)
                     frame = cv2.addWeighted(frame, 1.0, blk, 0.25, 1)
                     for t in range(len(text)):
                         cv2.putText(frame, text[t], coord[t], font, 0.25, (0, 0, 0), 1)
 
-            # save
-            out.write(frame)
+            # Save the frame to the list
+            gif_frames.append(frame)
 
-            if args.show:
-                # show
-                cv2.imshow('key-frame detection', frame)
-                cv2.waitKey(1)
+            # Save the frame as an image (optional)
+            #cv2.imwrite("/content/PyTorch_YOWO/img/output_image{0}.jpg".format(i), frame)
 
         else:
             break
 
+    # Release the video capture
     video.release()
-    out.release()
-    #cv2.destroyAllWindows()
+
+    # Save the frames as a gif using imageio
+    imageio.mimsave(save_name, gif_frames, duration=1/fps)
+
+    print(f'Gif saved at: {save_name}')
 
 
 if __name__ == '__main__':
