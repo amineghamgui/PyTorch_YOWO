8d04aed22a2030074bca0114595d26da8e4d504a
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-03-14 08:49:36 +0100

    Update distributed_utils.py

--- utils/distributed_utils.py
+++ utils/distributed_utils.py
@@ -142,21 +142,10 @@ def save_on_master(*args, **kwargs):
 
 
 def init_distributed_mode(args):
-    if 1==1:
-        # args.rank = int(os.environ["RANK"])
-        print("get_rank()",get_rank())
-        print("get_world_size()",get_world_size())
-        print("args.dist_url",args.dist_url)
-        args.world_size = 1
-        args.rank = 0
-        args.gpu = 1
-        # args.world_size = int(os.environ['WORLD_SIZE'])
-        # args.gpu = int(os.environ['LOCAL_RANK'])
-        # args.rank = 0
-        
-        # args.world_size = 1
-        
-         
+    if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+        args.rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ['WORLD_SIZE'])
+        args.gpu = int(os.environ['LOCAL_RANK'])
     elif 'SLURM_PROCID' in os.environ:
         args.rank = int(os.environ['SLURM_PROCID'])
         args.gpu = args.rank % torch.cuda.device_count()
@@ -164,7 +153,7 @@ def init_distributed_mode(args):
         print('Not using distributed mode')
         args.distributed = False
         return
-
+     
 
     args.distributed = True
 
