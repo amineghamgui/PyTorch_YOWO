bb936977ebc42cbc2be9f5281aa09376531bee75
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-03-14 09:49:34 +0100

    Update distributed_utils.py

--- utils/distributed_utils.py
+++ utils/distributed_utils.py
@@ -141,35 +141,27 @@ def save_on_master(*args, **kwargs):
         torch.save(*args, **kwargs)
 
 
-# def init_distributed_mode(args):
-#     if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
-#         args.rank = int(os.environ["RANK"])
-#         args.world_size = int(os.environ['WORLD_SIZE'])
-#         args.gpu = int(os.environ['LOCAL_RANK'])
-#     elif 'SLURM_PROCID' in os.environ:
-#         args.rank = int(os.environ['SLURM_PROCID'])
-#         args.gpu = args.rank % torch.cuda.device_count()
-#     else:
-#         print('Not using distributed mode')
-#         args.distributed = False
-#         return
-     
-
-#     args.distributed = True
-
-#     torch.cuda.set_device(args.gpu)
-#     args.dist_backend = 'nccl'
-#     print('| distributed init (rank {}): {}'.format(
-#         args.rank, args.dist_url), flush=True)
-#     torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
-#                                          world_size=args.world_size, rank=args.rank)
-#     torch.distributed.barrier()
-#     setup_for_distributed(args.rank == 0)
 def init_distributed_mode(args):
-    if args.rank is None or args.world_size is None:
-        print('Rank and world size information not provided, not using distributed mode.')
+    # if 'RANK' in os.environ and 'WORLD_SIZE' in os.environ:
+    #     args.rank = int(os.environ["RANK"])
+    #     args.world_size = int(os.environ['WORLD_SIZE'])
+    #     args.gpu = int(os.environ['LOCAL_RANK'])
+    # elif 'SLURM_PROCID' in os.environ:
+    #     args.rank = int(os.environ['SLURM_PROCID'])
+    #     args.gpu = args.rank % torch.cuda.device_count()
+    # else:
+    #     print('Not using distributed mode')
+    #     args.distributed = False
+    #     return
+
+    if 1==1:
+        args.rank = 0
+        args.gpu = args.rank % torch.cuda.device_count()
+    else:
+        print('Not using distributed mode')
         args.distributed = False
         return
+    
 
     args.distributed = True
 
@@ -177,12 +169,28 @@ def init_distributed_mode(args):
     args.dist_backend = 'nccl'
     print('| distributed init (rank {}): {}'.format(
         args.rank, args.dist_url), flush=True)
-    torch.distributed.init_process_group(
-        backend=args.dist_backend,
-        init_method=args.dist_url,
-        world_size=args.world_size,
-        rank=args.rank
-    )
+    torch.distributed.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                         world_size=args.world_size, rank=args.rank)
     torch.distributed.barrier()
     setup_for_distributed(args.rank == 0)
+# def init_distributed_mode(args):
+#     if args.rank is None or args.world_size is None:
+#         print('Rank and world size information not provided, not using distributed mode.')
+#         args.distributed = False
+#         return
+
+#     args.distributed = True
+
+#     torch.cuda.set_device(args.gpu)
+#     args.dist_backend = 'nccl'
+#     print('| distributed init (rank {}): {}'.format(
+#         args.rank, args.dist_url), flush=True)
+#     torch.distributed.init_process_group(
+#         backend=args.dist_backend,
+#         init_method=args.dist_url,
+#         world_size=args.world_size,
+#         rank=args.rank
+#     )
+#     torch.distributed.barrier()
+#     setup_for_distributed(args.rank == 0)
 
