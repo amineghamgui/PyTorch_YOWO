978168a91f63dd6f7571fe5f2569cd65e949ac1b
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-03-15 09:48:55 +0100

    Update dataset_config.py

--- config/dataset_config.py
+++ config/dataset_config.py
@@ -160,25 +160,28 @@ dataset_config = {
         'warmup_factor': 0.00066667,
         'wp_iter': 500,
         # class names
-        'valid_num_classes': 80,
-        'label_map': (
-                    'bend/bow(at the waist)', 'crawl', 'crouch/kneel', 'dance', 'fall down',  # 1-5
-                    'get up', 'jump/leap', 'lie/sleep', 'martial art', 'run/jog',             # 6-10
-                    'sit', 'stand', 'swim', 'walk', 'answer phone',                           # 11-15
-                    'brush teeth', 'carry/hold (an object)', 'catch (an object)', 'chop', 'climb (e.g. a mountain)',  # 16-20
-                    'clink glass', 'close (e.g., a door, a box)', 'cook', 'cut', 'dig',                               # 21-25
-                    'dress/put on clothing', 'drink', 'drive (e.g., a car, a truck)', 'eat', 'enter',                 # 26-30
-                    'exit', 'extract', 'fishing', 'hit (an object)', 'kick (an object)',                              # 31-35
-                    'lift/pick up', 'listen (e.g., to music)', 'open (e.g., a window, a car door)', 'paint', 'play board game',  # 36-40
-                    'play musical instrument', 'play with pets', 'point to (an object)', 'press','pull (an object)',             # 41-45
-                    'push (an object)', 'put down', 'read', 'ride (e.g., a bike, a car, a horse)', 'row boat',                   # 46-50
-                    'sail boat', 'shoot', 'shovel', 'smoke', 'stir',                                                             # 51-55
-                    'take a photo', 'text on/look at a cellphone', 'throw', 'touch (an object)', 'turn (e.g., a screwdriver)',   # 56-60
-                    'watch (e.g., TV)', 'work on a computer', 'write', 'fight/hit (a person)', 'give/serve (an object) to (a person)',  # 61-65
-                    'grab (a person)', 'hand clap', 'hand shake', 'hand wave', 'hug (a person)',                                        # 66-70
-                    'kick (a person)', 'kiss (a person)', 'lift (a person)', 'listen to (a person)', 'play with kids',                  # 71-75
-                    'push (another person)', 'sing to (e.g., self, a person, a group)', 'take (an object) from (a person)',             # 76-78
-                    'talk to (e.g., self, a person, a group)', 'watch (a person)'                                                       # 79-80
-                ),        
+        'valid_num_classes': 1,
+        # 'valid_num_classes': 80,
+        'label_map': ('bend/bow(at the waist)'),
+                    
+        # 'label_map': (
+        #             'bend/bow(at the waist)', 'crawl', 'crouch/kneel', 'dance', 'fall down',  # 1-5
+        #             'get up', 'jump/leap', 'lie/sleep', 'martial art', 'run/jog',             # 6-10
+        #             'sit', 'stand', 'swim', 'walk', 'answer phone',                           # 11-15
+        #             'brush teeth', 'carry/hold (an object)', 'catch (an object)', 'chop', 'climb (e.g. a mountain)',  # 16-20
+        #             'clink glass', 'close (e.g., a door, a box)', 'cook', 'cut', 'dig',                               # 21-25
+        #             'dress/put on clothing', 'drink', 'drive (e.g., a car, a truck)', 'eat', 'enter',                 # 26-30
+        #             'exit', 'extract', 'fishing', 'hit (an object)', 'kick (an object)',                              # 31-35
+        #             'lift/pick up', 'listen (e.g., to music)', 'open (e.g., a window, a car door)', 'paint', 'play board game',  # 36-40
+        #             'play musical instrument', 'play with pets', 'point to (an object)', 'press','pull (an object)',             # 41-45
+        #             'push (an object)', 'put down', 'read', 'ride (e.g., a bike, a car, a horse)', 'row boat',                   # 46-50
+        #             'sail boat', 'shoot', 'shovel', 'smoke', 'stir',                                                             # 51-55
+        #             'take a photo', 'text on/look at a cellphone', 'throw', 'touch (an object)', 'turn (e.g., a screwdriver)',   # 56-60
+        #             'watch (e.g., TV)', 'work on a computer', 'write', 'fight/hit (a person)', 'give/serve (an object) to (a person)',  # 61-65
+        #             'grab (a person)', 'hand clap', 'hand shake', 'hand wave', 'hug (a person)',                                        # 66-70
+        #             'kick (a person)', 'kiss (a person)', 'lift (a person)', 'listen to (a person)', 'play with kids',                  # 71-75
+        #             'push (another person)', 'sing to (e.g., self, a person, a group)', 'take (an object) from (a person)',             # 76-78
+        #             'talk to (e.g., self, a person, a group)', 'watch (a person)'                                                       # 79-80
+        #         ),        
     }
 }
