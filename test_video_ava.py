7eb0c92bdd0b15a4fd626145ccb4831cdee60b9e
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-03-18 14:43:18 +0100

    Update test_video_ava.py

--- test_video_ava.py
+++ test_video_ava.py
@@ -438,7 +438,17 @@ if __name__ == '__main__':
 
     # to eval
     model = model.to(device).eval()
+    import torch
 
+    # Assuming 'model' is already defined and loaded with weights, and set to evaluation mode
+    
+    # Define the path where you want to save the model
+    save_path = '/kaggle/input/yowonano/model.pth'
+    
+    # Save the model
+    torch.save(model.state_dict(), save_path)
+    
+    print("Model saved successfully at:", save_path)
     # run
     run(args=args, d_cfg=d_cfg, model=model, device=device,
         transform=basetransform, class_names=class_names)
