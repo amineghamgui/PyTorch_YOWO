302b23af37c9b1e8d0bef1feefb697c0e1c304f0
amineghamgui <125130818+amineghamgui@users.noreply.github.com>
2024-04-17 12:04:51 +0100

    Update ava_evaluator.py

--- evaluator/ava_evaluator.py
+++ evaluator/ava_evaluator.py
@@ -168,7 +168,7 @@ class AVA_Evaluator(object):
 
             # video = self.video_idx_to_name[video_idx]
             video = video_idx
-            key = str(video) + ',' + "%04d" % (sec)
+            key = "aaaaaaaaaaaaaaaaaa" + ',' + "%04d" % (sec)
             box = [box[1], box[0], box[3], box[2]]  # turn to y1,x1,y2,x2
 
             for cls_idx, score in enumerate(scores):
